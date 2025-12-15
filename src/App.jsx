12a4c467be68bcb421956a451d5c3d270cfaa2fa
shubh <shubhamw881@gmail.com>
2025-12-15 18:25:18 +0530

    feat: game board setup with style

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
-import React from "react";
+import "./App.css";
+import Board from "./game/components/Board";
+import Hud from "./game/components/Hud";
+import Overlay from "./game/components/Overlay";
+import { usePacmanGame } from "./game/hooks/usePacmanGame";
 
-const App = () => {
-  return <div>Pac-Man Rebuild</div>;
-};
+export default function App2() {
+  const { cells, gameState } = usePacmanGame();
 
-export default App;
+  return (
+    <div className="body">
+      <div id="header">Pac-man</div>
+      <Hud score={gameState.score} />
+      <Overlay gameStatus={gameState.gameStatus} />
+      <Board cells={cells} gameState={gameState} />
+    </div>
+  );
+}
